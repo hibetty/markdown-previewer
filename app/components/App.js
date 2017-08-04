310f424af31ff6e4c2058def46b7ee96728ffed6
Betty Lin <bettyhappylin@gmail.com>
2017-08-04 09:05:10 -0700

    set up beginning page structure, installed marked

--- app/components/App.js
+++ app/components/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(e){
+    this.setState({input: e.target.value});
+  }
+
   render() {
-    return <h1>Hello world</h1>;
+    return (
+      <div>
+        <textarea onChange={this.handleInput} value={this.state.value}></textarea>
+        <div>{this.state.input}</div>
+      </div>
+    );
   }
 }
