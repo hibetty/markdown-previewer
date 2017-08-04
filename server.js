93ed6c1d881afffbd3c80928767d39259577fd92
Betty Lin <bettyhappylin@gmail.com>
2017-08-04 13:33:47 -0700

    added express and updated start script for heroku deploy

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+let app = express();
+app.use(express.static('build'));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log('Express server running on' + PORT));
